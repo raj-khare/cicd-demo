4a812542b66ea7f0b6b8fbe090a0da364d98b869
Raj Khare <seven.triangles@icloud.com>
2019-11-26 14:22:48 +0530

    Update docker script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG .
-docker push $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG
+docker build -f Dockerfile -t $DOCKER_USER/cicd:$TAG .
+docker push $DOCKER_USER/cicd:$TAG
